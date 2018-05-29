fa23c686a2dad3e0f5fbf0110ac9fd6df5d7497e
SundayLPA <31652823+SundayLPA@users.noreply.github.com>
2018-05-29 10:55:47 +0800

    add comment
    start the delete_nth.py

---
+++ delete_nth.py
@@ -0,0 +1,9 @@
+"""
+Given a list and a number N, create a new list that contains each number of the list at most N times without reordering.
+"""
+
+import collections
+
+#Time complexity O(n^2)
+
+def 
